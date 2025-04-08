a925886e543be44b43dd641c9379acdc33aa4e0d
Takunda Sylvester Dube <takundaslydube@gmail.com>
2025-04-08 15:37:56 +0200

    migrations

---
+++ migrations/20250407081147-users.js
@@ -0,0 +1,64 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+        user_id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            unique: true,
+            primaryKey: true,
+            allowNull: false,
+        },
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        last_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        phone_number: {
+            type: DataTypes.STRING,
+            unique: true,
+            allowNull: false,
+        },
+        role_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: "roles",
+                key: "role_id",
+                onDelete: "RESTRICT",
+                onUpdate: "CASCADE"
+            }
+        },
+        is_verified: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: false,
+        },
+        is_active: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: true,
+        },
+
+  });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
